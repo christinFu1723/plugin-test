6e093c5dc001b9daf22e093413b5a9396c15ebaf
fwl <172396333@qq.com>
2022-08-15 17:33:10 +0800

    feat: 修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  // 强制内联CSS
+  css: {
+    extract: false,
+  },
+  // 扩展 webpack 配置，使 components 加入编译
+  chainWebpack: (config) => {
+    config.module
+      .rule("js")
+      .include.add("/plugin") //这里根据你文件夹名称自定义，每个人不一样
+      .end()
+      .use("babel")
+      .loader("babel-loader")
+      .tap((options) => {
+        // 修改它的选项...
+        return options;
+      });
+  },
+});
