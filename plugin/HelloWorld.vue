0126fcb1e9f077679eba0693e26c21334b382f08
fwl <172396333@qq.com>
2022-08-15 15:40:48 +0800

    feat: 追加3方插件

--- plugin/HelloWorld.vue
+++ plugin/HelloWorld.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="hello">文立测试包</div>
+  <div>
+    <div class="hello">文立测试包</div>
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+    </el-date-picker>
+  </div>
 </template>
 
 <script>
+import { Input, DatePicker } from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
 export default {
   name: "HelloWorld",
+  components: {
+    "el-input": Input,
+    "el-date-picker": DatePicker,
+  },
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      input: "",
+      value1: "",
+    };
   },
 };
 </script>
